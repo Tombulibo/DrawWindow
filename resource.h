b53f98102fc7ba15c36b4e710156abd484615251
Tombulibo <chinachengyuantong@163.com>
2021-10-06 13:54:26 +0800

    添加项目文件。

---
+++ resource.h
@@ -0,0 +1,31 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 DrawWindow.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_DRAWWINDOW_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_STARFISH                    106
+#define IDI_DRAWWINDOW                  107
+#define IDI_SMALL                       108
+#define IDC_DRAWWINDOW                  109
+#define IDR_MAINFRAME                   128
+#define IDC_CURSOR1                     129
+#define IDC_SHARK                       129
+#define IDI_SHARK                       134
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
